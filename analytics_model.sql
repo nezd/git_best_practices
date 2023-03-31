1d722c71f560bf7f2810406e485a3e11fe2184df
Yordan Ivanov <mail@ivanovyordan.com>
2023-03-31 15:58:23 +0300

    Change analytics model date limits
    blah blah

--- analytics_model.sql
+++ analytics_model.sql
@@ -2,4 +2,4 @@ select
     *
 from analytics.prepare.table_A
 where ID in (231434, 3452345, 645466)
-and created_at > to_date('2022-12-23')
+and created_at between to_date('2022-12-23') and to_date('2023-03-31')
